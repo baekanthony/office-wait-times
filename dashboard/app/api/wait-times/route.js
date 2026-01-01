e92cd6675aed37c981bc48b77673da369d4f7053
baekanthony <baekanthony@gmail.com>
2025-12-31 21:29:34 -0800

    modify dynamoDB fetch to allow fetch all

--- dashboard/app/api/wait-times/route.js
+++ dashboard/app/api/wait-times/route.js
@@ -1,5 +1,5 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, QueryCommand, ScanCommand } from "@aws-sdk/lib-dynamodb";
 import { NextResponse } from 'next/server';
 
 const region = process.env.REGION;
@@ -13,15 +13,20 @@ export async function GET(request) {
   const officeId = searchParams.get('officeId');
 
   try {
-    const params = {
-      TableName,
-      KeyConditionExpression: 'officeId = :officeId',
-      ExpressionAttributeValues: {
-        ':officeId': officeId,
-      },
-    };
-    const result = await docClient.send(new QueryCommand(params));
-    return NextResponse.json({ items: result.Items });
+    if (officeId) {
+      const params = {
+        TableName,
+        KeyConditionExpression: 'officeId = :officeId',
+        ExpressionAttributeValues: {
+          ':officeId': officeId,
+        },
+      };
+      const result = await docClient.send(new QueryCommand(params));
+      return NextResponse.json({ items: result.Items });
+    } else {
+      const result = await docClient.send(new ScanCommand({ TableName }));
+      return NextResponse.json({ items: result.Items });
+    }
   } catch (err) {
     return NextResponse.json({ error: err.message }, { status: 500 });
   }
